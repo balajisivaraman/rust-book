7905208068ea67fc1d270952f4744a90610339b3
Balaji Sivaraman <balaji@balajisivaraman.com>
2017-10-01 15:50:35 +0530

    Initial implementation of ThreadPool with workers

--- hello/src/lib.rs
+++ hello/src/lib.rs
@@ -1,8 +1,40 @@
-pub struct ThreadPool;
+use std::thread;
+
+pub struct ThreadPool {
+    workers: Vec<Worker>,
+}
+
+struct Worker {
+    id: usize,
+    thread: thread::JoinHandle<()>,
+}
+
+impl Worker {
+    fn new(id: usize) -> Worker {
+        let thread = thread::spawn(|| {});
+
+        Worker { id, thread }
+    }
+}
 
 impl ThreadPool {
-    pub fn new(size: u32) -> ThreadPool {
-        ThreadPool
+    /// Create a new ThreadPool.
+    ///
+    /// The size is the number of threads in the pool.
+    ///
+    /// # Panics
+    ///
+    /// The `new` function will panic if the size is zero.
+    pub fn new(size: usize) -> ThreadPool {
+        assert!(size > 0);
+
+        let mut workers = Vec::with_capacity(size);
+
+        for id in 0..size {
+            workers.push(Worker::new(id))
+        }
+
+        ThreadPool { workers }
     }
 
     pub fn execute<F>(&self, f: F)
